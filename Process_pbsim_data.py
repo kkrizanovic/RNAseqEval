c1b7534634c0840d8833fa12264efa8e8bfecca8
Krešimir Križanović <kkrizanovic@genome.zesoi.fer.hr>
2017-02-16 20:07:20 +0100

    Small improvements

--- Process_pbsim_data.py
+++ Process_pbsim_data.py
@@ -32,12 +32,22 @@ simFolderDict_d2 = {'SimG1AS' : 'group1_AS'
                   , 'SimG3AS' : 'group3_AS'
                   , 'SimG3SS' : 'group3_SS'}
 
+simFolderDict_all = {'SimG1' : 'group1'
+                   , 'SimG2' : 'group2'
+                   , 'SimG3' : 'group3'
+                   , 'SimG1AS' : 'group1_AS'
+                   , 'SimG1SS' : 'group1_SS'
+                   , 'SimG2AS' : 'group2_AS'
+                   , 'SimG2SS' : 'group2_SS'
+                   , 'SimG3AS' : 'group3_AS'
+                   , 'SimG3SS' : 'group3_SS'}
+
 # A dictionary connecting fasta/fastq header prefix with the folder with pbsim generated data
 # Containing information for reads with each prefix
 # This is used because data is simulated using several pbsim runs to get different
 # coverages for different sets of references (in this case transcripts)
 # NOTE: this should be changed for different simulations
-simFolderDict = simFolderDict_d2
+simFolderDict = simFolderDict_all
 
 
 def interval_equals(interval1, interval2, allowed_inacc = Annotation_formats.DEFAULT_ALLOWED_INACCURACY):
